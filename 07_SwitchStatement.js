7ed1aed700a5d2780e40bcecc70208a9b3663f3a
Andy Faulkner <andrewfaulkner80@gmail.com>
2016-06-14 14:26:27 +0100

    Update

--- 07_SwitchStatement.js
+++ 07_SwitchStatement.js
@@ -3,34 +3,44 @@ document.addEventListener('load',dateTime(),false);
 
 //Create a Date and Time switch function
 function dateTime() {
-    
     var day = "";
     var today = new Date();
+    var hour = today.getHours();
+    var mins = today.getMinutes();
+    var checkZero = "";
     
+//check if minutes less than 10 and change to two digit format if so
+    if(mins < 10){
+        checkZero = 0;
+    }
+    
+//Switch through days
     switch (today.getDay()){
         case 0:
-            document.getElementById("date").innerHTML = "Today is Sunday";
+            day = "Sunday";
             break;
         case 1:
-            document.getElementById("date").innerHTML = "Today is Monday";
+            day = "Monday";
             break;
         case 2:
-            document.getElementById("date").innerHTML = "Today is Tuesday";
+            day = "Tuesday";
             break;
         case 3:
-            document.getElementById("date").innerHTML = "Today is Wednesday";
+            day = "Wednesday";
             break;
         case 4:
-            document.getElementById("date").innerHTML = "Today is Thursday";
+           day = "Thursday";
             break;
         case 5:
-            document.getElementById("date").innerHTML = "Today is Friday";
+            day = "Friday";
             break;
         case 6:
-            document.getElementById("date").innerHTML = "Today is Saturday";
+            day = "Saturday";
             break;
         default:
-            document.getElementById("date").innerHTML = "Today is not a day!";
+            day = "not a day";
             break;
     }
+    //Print to screen
+document.getElementById("time").innerHTML = "Today is " + day + " and the time is " + hour + ":" + checkZero + mins;
 }
